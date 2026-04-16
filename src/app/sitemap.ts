a2a866aad57fb53a03a5279924f1bdc9a9909e94
Sense9961 <sense9961@gmail.com>
2026-04-17 00:43:09 +0700

    feat: update sitemap with quote, services, materials routes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,30 @@
 import { MetadataRoute } from 'next'
-
+ const lastModified = new Date()
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
             url: 'https://www.additiv3.com/',
-            lastModified: new Date(),
+            lastModified,
             changeFrequency: 'monthly',
             priority: 1,
+        },
+        {
+            url: `https://www.additiv3.com/quote`,
+            lastModified,
+            changeFrequency: 'monthly',  // Cứ để monthly, không sao
+            priority: 0.9,
+        },
+        {
+            url: `https://www.additiv3.com/services`,
+            lastModified,
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: `https://www.additiv3.com/materials`,
+            lastModified,
+            changeFrequency: 'monthly',
+            priority: 0.8,
         },
     ]
 }
